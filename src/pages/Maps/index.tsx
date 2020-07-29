480536c41a47ce4d878edf38c2cf75c35e155a9c
robsonAraujo <robsonaraujocarmo@hotmail.com>
2020-07-28 23:07:32 -0300

    implemented apple maps and get user lat long

---
+++ src/pages/Maps/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { View } from 'react-native';
+import { Title } from './styles';
+
+const Maps: React.FC = () => {
+  const latitudeDelta = 0.0922;
+  const longitudeDelta = 0.0421;
+  const [coordinate, setCoordinate] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta,
+    longitudeDelta,
+  });
+  return (
+    <>
+      <MapView
+        style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+        region={coordinate}
+        showsUserLocation
+        onUserLocationChange={e => {
+          const { latitude, longitude } = e.nativeEvent.coordinate;
+
+          setCoordinate({
+            latitude,
+            longitude,
+            longitudeDelta,
+            latitudeDelta,
+          });
+        }}
+      >
+        <Marker
+          draggable
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="meu title"
+          description="minha description"
+          onPress={e => console.log('oi', e.nativeEvent)}
+        />
+      </MapView>
+    </>
+  );
+};
+
+export default Maps;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Maps from '../pages/Maps';
 
 const Auth = createStackNavigator();
 
@@ -14,6 +15,7 @@ const AuthRoutes: React.FC = () => (
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
+    <Auth.Screen name="Maps" component={Maps} />
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
   </Auth.Navigator>
